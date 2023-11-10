1464882963de029e735f5c3ca809c0dce140881c
Yosef <yosethio@yahoo.com>
2023-11-10 14:18:21 +0300

    update againe

--- app/(dashboard)/_components/chat-with-group/chat-with-group.tsx
+++ app/(dashboard)/_components/chat-with-group/chat-with-group.tsx
@@ -51,7 +51,7 @@ export default function ChatWithGroup() {
             _creationTime={group._creationTime ?? 0}
             avatarUrl={group.avatarUrl ?? " "}
             isActive={activeGroupId === group._id}
-            {/* @ts-ignore */}
+            // @ts-ignore 
             onClick={() => handleCardClick(group)}
           />
         );
