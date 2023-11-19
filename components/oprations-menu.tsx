7a43e070e5475bc9208d5404748fc00863b0d3b0
Yosef <yosethio@yahoo.com>
2023-11-19 03:48:22 +0300

    delete hook issue

--- components/oprations-menu.tsx
+++ components/oprations-menu.tsx
@@ -24,22 +24,53 @@ interface PostProps {
 }
 
 
+export function JournalOperationsMenu({ id }: PostProps) {
+  const { deleteJournal } = useDeleteJournal(id as Id<"journals">);
+  const handleDelete = () => {
+    deleteJournal();
+  }
+
+  return (
+    <>
+      <DropdownMenu>
+        {/* Dropdown menu content */}
+        <DropdownMenuItem
+          className="flex cursor-pointer items-center text-destructive focus:text-destructive"
+          onSelect={handleDelete}
+        >
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenu>
+    </>
+  )
+}
+
+export function TaskOperationsMenu({ id }: PostProps) {
+  const { deleteTask } = useDeleteTasks(id as Id<"tasks">);
+  const handleDelete = () => {
+    deleteTask();
+  }
+
+  return (
+    <>
+      <DropdownMenu>
+        {/* Dropdown menu content */}
+        <DropdownMenuItem
+          className="flex cursor-pointer items-center text-destructive focus:text-destructive"
+          onSelect={handleDelete}
+        >
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenu>
+    </>
+  )
+}
+
 export function OperationsMenu({ id, identity }: PostProps) {
   const router = useRouter();
   const params = useParams();
   const [journalHook, setJournalHook] = React.useState(null);
   const [taskHook, setTaskHook] = React.useState(null);
-
-  const { deleteJournal } = useDeleteJournal(id as Id<"journals">);
-  const { deleteTask } = useDeleteTasks(id as Id<"tasks">);
-
-  const handleDelete = (identity: string) => {
-    if (identity === 'journal') {
-      deleteJournal();
-    } else {
-      deleteTask();
-    }
-  }
 
   return (
     <>
@@ -55,12 +86,11 @@ export function OperationsMenu({ id, identity }: PostProps) {
             </Link>
           </DropdownMenuItem>
           <DropdownMenuSeparator />
-          <DropdownMenuItem
-            className="flex cursor-pointer items-center text-destructive focus:text-destructive"
-            onSelect={() => handleDelete(identity)}
-          >
-            Delete
-          </DropdownMenuItem>
+          {identity === 'journal' ? (
+            <JournalOperationsMenu id={id} />
+          ) : (
+            <TaskOperationsMenu id={id} />
+          )}
         </DropdownMenuContent>
       </DropdownMenu>
     </>
