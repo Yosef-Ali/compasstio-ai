45e2da9c52abc85a8dbf7ea64e454d98ad0b7908
Yosef <yosethio@yahoo.com>
2023-11-21 05:46:50 +0300

    Top nav update

--- app/(dashboard)/_components/top-nav/top-nav-livesection.tsx
+++ app/(dashboard)/_components/top-nav/top-nav-livesection.tsx
@@ -1,20 +1,11 @@
-import { useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
-import { Id } from "@/convex/_generated/dataModel";
 
-interface TitleProps {
-  journalId: Id<"journals">;
-}
+
 
 const TopNavLiveSections = () => {
-
-
-
   return (
-    <h1>"Untitled"</h1>
+    <h1>&ldquo;Untitled&rdquo;</h1>
   );
-
 };
 
-export default TopNavLiveSections
+export default TopNavLiveSections;
 
