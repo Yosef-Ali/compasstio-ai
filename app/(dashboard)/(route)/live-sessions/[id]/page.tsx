7e981251866cb2aeb05eda9b44ea26aa9b949e6f
Yosef-Ali <yosethio@yahoo.com>
2024-02-06 12:04:57 +0300

    dynamic page and  dotenv

--- app/(dashboard)/(route)/live-sessions/[id]/page.tsx
+++ app/(dashboard)/(route)/live-sessions/[id]/page.tsx
@@ -14,10 +14,6 @@ import { Spinner } from "@/components/spinner";
 import { api } from "@/convex/_generated/api";
 import { useMutation, useQuery } from "convex/react";
 import { useUser } from "@clerk/nextjs";
-import JoinScreen from "@/app/(dashboard)/_components/live-streaming/join-screen";
-import VideoComponent from "@/app/(dashboard)/_components/live-streaming/video-component";
-import { ArrowRightIcon } from "lucide-react";
-import { useSlideState } from "@/app/hooks/useSlideState";
 
 
 type ParticipantsViewer = {
@@ -27,78 +23,30 @@ type ParticipantsViewer = {
 }
 
 export const dynamic = 'force-dynamic'
-// function JoinScreen({
-//   getMeetingAndToken,
-// }: {
-//   getMeetingAndToken: (meeting?: string) => void,
-// }) {
-//   const [meetingId, setMeetingId] = useState<string | undefined>();
-//   const onClick = async () => {
-//     getMeetingAndToken(meetingId);
-//   };
-//   return (
-//     <div>
-//       <input
-//         type="text"
-//         placeholder="Enter Meeting Id"
-//         onChange={(e) => {
-//           setMeetingId(e.target.value);
-//         }}
-//       />
-//       <button onClick={onClick}>Join</button>
-//       {" or "}
-//       <button onClick={onClick}>Create Meeting</button>
-//     </div>
-//   );
-// }
-
 function JoinScreen({
   getMeetingAndToken,
 }: {
   getMeetingAndToken: (meeting?: string) => void,
 }) {
   const [meetingId, setMeetingId] = useState<string | undefined>();
-  const [localMode, setLocalMode] = useState<"CONFERENCE" | "VIEWER" | undefined>("CONFERENCE");
-  const { isSlideOut, toggleSlide } = useSlideState();
-
-  const onClick = async (selectedMode: "CONFERENCE" | "VIEWER") => {
-    setLocalMode(selectedMode);
-    // Call getMeetingAndToken only if meetingId is defined
-    if (meetingId) {
-      await getMeetingAndToken(meetingId);
-    } else {
-      // If no meetingId is provided, create a new meeting
-      //@ts-ignore
-      await getMeetingAndToken();
-    }
+  const onClick = async () => {
+    getMeetingAndToken(meetingId);
   };
-
   return (
-    < div key="1" className="min-h-screen flex flex-col md:flex-row bg-gray-800" >
-      <div className="flex flex-1 flex-col md:flex-row items-center justify-center md:m-[72px] m-16">
-        <div className="container grid  md:grid-flow-col grid-flow-row ">
-          <div className="grid grid-cols-12">
-            <VideoComponent />
-            <div className="md:col-span-5 2xl:col-span-6 col-span-12 md:relative">
-              <div className="flex flex-1 flex-col items-center justify-center xl:m-16 lg:m-6 md:mt-9 lg:mt-14 xl:mt-20 mt-3 md:absolute md:left-0 md:right-0 md:top-0 md:bottom-0">
-                <div className={`w-full text-white flex flex-col gap-4 flex-1 items-center justify-center xl:m-10  md:mt-11  ${isSlideOut ? "md:mx-48" : ""} `}>
-                  <Button onClick={() => onClick("CONFERENCE")} className="w-full bg-purple-500  whitespace-nowrap">
-                    Create a Meeting
-                  </Button>
-                  <Button className=" rounded-md px-4 py-2 flex items-center justify-center w-full whitespace-nowrap cursor-default text-gray-400">
-                    Join the Meeting from the List
-                    <ArrowRightIcon className="ml-2 h-4 w-4" />
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div >
+    <div>
+      <input
+        type="text"
+        placeholder="Enter Meeting Id"
+        onChange={(e) => {
+          setMeetingId(e.target.value);
+        }}
+      />
+      <button onClick={onClick}>Join</button>
+      {" or "}
+      <button onClick={onClick}>Create Meeting</button>
+    </div>
   );
 }
-
 
 
 function ParticipantView({ participantId, videoHeight, meetingId }: ParticipantsViewer) {
@@ -326,7 +274,7 @@ function Page() {
       <MeetingView meetingId={meetingId} onMeetingLeave={onMeetingLeave} />
     </MeetingProvider>
   ) : (
-    <JoinScreen getMeetingAndToken={getMeetingAndToken} setMode={setMode} />
+    <JoinScreen getMeetingAndToken={getMeetingAndToken} />
     // <p>Loading...</p>
   );
 }
