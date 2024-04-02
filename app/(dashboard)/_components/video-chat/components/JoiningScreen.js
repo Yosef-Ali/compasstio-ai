11bf2dd871d68dff73dfeaedf6999175ba9e5ebe
Yosef-Ali <yosethio@yahoo.com>
2024-04-02 10:11:18 +0300

    joining sereen camera off

--- app/(dashboard)/_components/video-chat/components/JoiningScreen.js
+++ app/(dashboard)/_components/video-chat/components/JoiningScreen.js
@@ -332,7 +332,7 @@ export function JoiningScreen({
   }, [videoTrack, setting, settingDialogueOpen]);
 
   useEffect(() => {
-    getDevices({ micEnabled, webcamEnabled });
+    getDevices({ micEnabled: false, webcamEnabled: false });
   }, []);
 
   return (
