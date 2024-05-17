209055e71c8b16624d7f44be70e2493194fb9cd0
Yosef-Ali <yosethio@yahoo.com>
2024-05-17 10:27:40 +0300

    update

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -9,17 +9,17 @@ import Image from "next/image"
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={cn(className)}>
-      <p className="text-md  text-white/70 p-4 text-center">As it is imperative to build community, a percentage of net profit will be donated to philanthropy at our
+      <p className="text-md  text-white/70 p-2 text-center">As it is imperative to build community, a percentage of net profit will be donated to philanthropy at our
         discretion.</p>
-      <div className="container flex flex-col items-center justify-between gap-4 z-50  md:flex-row">
+      <div className="container flex flex-col items-center justify-between gap-3 z-50  md:flex-row">
 
-        <div className="flex items-center gap-2 leading-">
+        <div className="flex items-center gap-2 leading-3">
           <Image src="./logo.svg" alt="Logo" width={30} height={30} />
           <span className="font-bold sm:inline-block text-xl text-white/70">
             {siteConfig.name}
           </span>
         </div>
-        <div className="flex items-center justify-between gap-4 ">
+        <div className="flex items-center justify-between gap-2 ">
           <p className="text-center md:text-left text-sm  text-white/70">
             Built by{" "}
             <a
