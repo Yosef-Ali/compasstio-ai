33778c1ba2fccdafad18693da57e6fe0541fcdcd
Yosef-Ali <yosethio@yahoo.com>
2024-05-17 17:27:10 +0300

    footer adjusted

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -2,24 +2,23 @@ import * as React from "react"
 
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
-import { Icons } from "@/components/icons"
 import { ModeToggle } from "@/components/mode-toggle"
 import Image from "next/image"
 
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={cn(className)}>
-      <p className="text-md  text-white/70 p-2 text-center">As it is imperative to build community, a percentage of net profit will be donated to philanthropy at our
+      <p className="text-md  text-white/70 p-2 text-center -mt-2 md:m-0">As it is imperative to build community, a percentage of net profit will be donated to philanthropy at our
         discretion.</p>
       <div className="container flex flex-col items-center justify-between gap-3 z-50  md:flex-row">
 
-        <div className="flex items-center gap-2 leading-5">
+        <div className="flex items-center gap-2">
           <Image src="./logo.svg" alt="Logo" width={30} height={30} />
           <span className="font-bold sm:inline-block text-xl text-white/70">
             {siteConfig.name}
           </span>
         </div>
-        <div className="flex items-center justify-between gap-2 pb-1 ">
+        <div className="flex items-center justify-between gap-2 pb-1">
           <p className="text-center md:text-left text-sm  text-white/70">
             Built by{" "}
             <a
